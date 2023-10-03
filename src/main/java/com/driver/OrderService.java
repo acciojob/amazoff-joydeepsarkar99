e2cf48ab6c39ab47549e4fb35a64b6e5c9b29a13
joydeepsarkar99 <jdsarkar99@gmail.com>
2023-10-03 23:31:12 +0530

    3rd attempt

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -9,8 +9,7 @@ import java.util.List;
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+    OrderRepository orderRepository = new OrderRepository();
 
     public void addOrder(Order order){
         orderRepository.addOrderToDB(order);
